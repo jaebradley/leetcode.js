a463d067cea38db3b58de4ac56bc21f1262fddc1
Jae Bradley <jae.b.bradley@gmail.com>
2018-07-10 09:00:46 -0700

    fix linting

--- src/valid-parentheses.test.js
+++ src/valid-parentheses.test.js
@@ -1,31 +1,31 @@
 var isValid = require('./valid-parentheses');
 
-describe('#isValid', () => {
-  it('is invalid parentheses for single open parentheses', () => {
+describe('#isValid', function() {
+  it('is invalid parentheses for single open parentheses', function() {
     expect(isValid('(')).toBe(false);
   });
 
-  it('is invalid parentheses for single closed parentheses', () => {
+  it('is invalid parentheses for single closed parentheses', function() {
     expect(isValid(')')).toBe(false);
   });
 
-  it('is valid parentheses for single pair', () => {
+  it('is valid parentheses for single pair', function() {
     expect(isValid('()')).toBe(true);
   });
 
-  it('is valid parentheses for multiple non-nested pairs in a row', () => {
+  it('is valid parentheses for multiple non-nested pairs in a row', function() {
     expect(isValid('(){}[]')).toBe(true);
   });
 
-  it('is valid parenthese for nested pairs', () => {
+  it('is valid parenthese for nested pairs', function() {
     expect(isValid('({})')).toBe(true);
   });
 
-  it('is not valid parentheses for invalid nested pairs', () => {
+  it('is not valid parentheses for invalid nested pairs', function() {
     expect(isValid('({))')).toBe(false);
   });
 
-  it('is valid for empty string', () => {
+  it('is valid for empty string', function() {
     expect(isValid('')).toBe(true);
   })
 });
