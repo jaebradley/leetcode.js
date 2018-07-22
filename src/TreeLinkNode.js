167e44283df71949a8c03b79d485dc3ce5fbea9e
Jae Bradley <jae.b.bradley@gmail.com>
2018-07-21 17:41:16 -0700

    next right pointers

---
+++ src/TreeLinkNode.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for binary tree with next pointer.
+ * function TreeLinkNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = this.next = null;
+ * }
+ */
+
+function TreeLinkNode(val) {
+  this.val = val;
+  this.left = this.right = this.next = null;
+}
+
+module.exports = TreeLinkNode;
