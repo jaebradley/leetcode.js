a5086e0c0b69f57044d782096dda721c1c6ef4ed
Jae Bradley <jae.b.bradley@gmail.com>
2018-10-24 07:44:58 -0700

    combination sum 2

--- src/combination-sum-2.test.js
+++ src/combination-sum-2.test.js
@@ -20,4 +20,39 @@ describe('combinationSum2', function() {
       expect(combinationSum2([10,1,2,7,6,1,5], 8)).toEqual(expected);
     });
   });
+
+  describe('when no candidates', function() {
+    it('empty result set', function() {
+      expect(combinationSum2([], 1)).toEqual([]);
+    });
+  })
+
+  describe('when candidates exist', function() {
+    describe('when duplicate solutions exist', function() {
+      it('returns unique result', function() {
+        var expected = [
+          [1, 1, 2]
+        ];
+        expect(combinationSum2([1, 1, 1, 2], 4)).toEqual(expected);
+      });
+    });
+
+    describe('when duplicate solutions do not exist', function() {
+      it('returns single result', function() {
+        var expected = [
+          [1, 1, 2]
+        ];
+        expect(combinationSum2([1, 1, 2], 4)).toEqual(expected);
+      });
+    });
+
+    describe('when backtracking occurs', function() {
+      it('returns single result', function() {
+        var expected = [
+          [1, 5]
+        ];
+        expect(combinationSum2([20, 1, 2, 5], 6)).toEqual(expected);
+      });
+    });
+  })
 });
