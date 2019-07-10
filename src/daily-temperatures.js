6eb6ab7fb55c3d449651de5b362e9f4aff5fb92d
Jae Bradley <jae.b.bradley@gmail.com>
2019-07-09 22:26:00 -0700

    Write tests

--- src/daily-temperatures.js
+++ src/daily-temperatures.js
@@ -1,4 +1,6 @@
 /**
+https://leetcode.com/problems/daily-temperatures/
+
 Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature.
 If there is no future day for which this is possible, put 0 instead.
 
@@ -25,7 +27,7 @@ function dailyTemperatures(temperatures) {
 
   var stack = [];
   temperatures.forEach(function (temperature, index) {
-    while (stack.length && temperature > stack[stack.length - 1]) {
+    while (stack.length && temperature > temperatures[stack[stack.length - 1]]) {
       var indexToUpdate = stack.pop();
       answers[indexToUpdate] = index - indexToUpdate;
     }
